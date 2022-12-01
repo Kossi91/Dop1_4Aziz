a637540786d37ffe49920970bf8ee10436072e39
Kossi91 <kossi.91.ai@gmail.com>
2022-12-01 19:05:38 +0600

    Initial commit

---
+++ app/src/main/java/com/example/concatadaper/ui/activity/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.concatadaper.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.concatadaper.R
+import com.example.concatadaper.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding?.btmNav?.let { bottomNavigation ->
+            NavigationUI.setupWithNavController(bottomNavigation, navController)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
